fea7b6a5d880372bbd4decdf965a5ecd5672fd60
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-03-12 12:53:17 +0200

    default language hardcoded to skip flickering

--- public/js/app/translate-config.js
+++ public/js/app/translate-config.js
@@ -4,12 +4,17 @@
     angular
     .module('app')
     .config(['$translateProvider', function ($translateProvider) {
+
+        //disable flicker on load, by creating default values
+        /*$translateProvider.translations('en', {
+            'BUTTON_LANG': ''
+        });*/
+
         $translateProvider.useStaticFilesLoader({
             'prefix': 'localization/locale-',
             'suffix': '.json'
         });
         $translateProvider.useSanitizeValueStrategy('escape');
-        //$translateProvider.preferredLanguage('en');
         $translateProvider.registerAvailableLanguageKeys(['en', 'et'], {
             'en_*': 'en',
             'en-*': 'en',
@@ -17,10 +22,13 @@
             'et-*': 'et',
         });
 
+        $translateProvider.preferredLanguage('en');
+
         $translateProvider.uniformLanguageTag('bcp47').determinePreferredLanguage();
         //http://www.loc.gov/standards/iso639-2/php/code_list.php
         //https://msdn.microsoft.com/et-ee/library/ms533052.aspx
         // enable BCP-47, must be before determinePreferredLanguage!
+
         $translateProvider.fallbackLanguage('en');
 
     }]);
