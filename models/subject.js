e49eaee32b2242132b29f5341e6437b6a22c5f75
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-06-11 12:13:27 +0300

    added translation option to model

--- models/subject.js
+++ models/subject.js
@@ -3,6 +3,8 @@ var Schema = mongoose.Schema;
 
 var subjectSchema = new Schema({
   name: {type: String, required: true, unique: true },
+  name_et: {type: String, required: true },
+  name_en: {type: String, required: true },
   page_views: { type: Number, default: 0 },
   following_count: { type: Number, default: 0 },
 });
