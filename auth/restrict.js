105506f5ae379654a3d28dbf905820b18b7b2d2f
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-07-26 13:58:11 +0300

    restrict now returns 403 with message if user changed

--- auth/restrict.js
+++ auth/restrict.js
@@ -1,11 +1,11 @@
 module.exports = function(req, res, next) {
 
   // check if same user as in session, when saving content to user profile
-  if(typeof req.body.user !== 'undefined' && typeof req.body.user._id !== 'undefined'){
-    //console.log('check');
-    if(req.body.user._id != req.user._id){
-      //console.log("new user:"+req.body.user._id+" changed old user:"+req.user._id);
-      return res.json({error: {id: 100, message: "refresh page, user changed"}});
+  if(req.body.user && req.body.user._id){
+    //console.log('check ' + req.body.user._id);
+    if(req.body.user._id !== req.user._id.toString()){
+      // console.log("new user:"+req.body.user._id+" changed old user:"+req.user._id);
+      return res.status(403).send("refresh page, user changed");
     }
   }
 
