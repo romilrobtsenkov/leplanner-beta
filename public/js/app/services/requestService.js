ad8a874ae576b7389ffdd1c313855a50c60cfd96
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-07-26 13:55:57 +0300

    included forbidden check in frontend request service

--- public/js/app/services/requestService.js
+++ public/js/app/services/requestService.js
@@ -3,20 +3,33 @@
 
     angular
     .module('app')
-    .factory('requestService', ['$http', function($http) {
+    .factory('requestService', ['$http', '$rootScope', '$location', '$q', function($http, $rootScope, $location, $q) {
         var API_BASE = '/api';
+
+        var success = function (response) { return response.data; };
+
         return {
             get: function(request) {
-                return $http.get(API_BASE + request)
-                .then(function(response) {
-                    return response.data;
-                });
+                return $http.get(API_BASE + request).then(success);
             },
 
             post: function(request, query) {
-                return $http.post(API_BASE + request, query)
-                .then(function(response) {
-                    return response.data;
+
+                // Add user id to compare with server session
+                if ($rootScope.user && $rootScope.user._id) {
+                    if (!query) { query = {}; }
+                    query.user = {
+                        _id: $rootScope.user._id
+                    };
+                }
+
+                return $http.post(API_BASE + request, query).then(success)
+                .catch(function(error){
+                    if (error.status === 403 && error.data === 'refresh page, user changed') {
+                        $location.path('/');
+                        return;
+                    }
+                    throw error;
                 });
             }
         };
