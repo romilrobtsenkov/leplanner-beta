d5e56dcc70d52721ff9e5de3743746c0e8775b79
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-08-01 18:01:27 +0300

    added 401 error to subclass errors

--- errors.js
+++ errors.js
@@ -2,5 +2,6 @@ const SubclassError = require('subclass-error');
 
 exports.Error = SubclassError("Error", {statusCode:400});
 
+exports.NotAuthorizedError = SubclassError("NotAuthorizedError", exports.Error, {statusCode:401});
 exports.ForbiddenError = SubclassError("ForbiddenError", exports.Error, {statusCode:403});
 exports.NotFoundError = SubclassError("NotFoundError", exports.Error, {statusCode:404});
