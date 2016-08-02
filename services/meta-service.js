995c85260e05235066b58e0f9d6d0d942014aa83
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-08-02 08:27:44 +0300

    removed old meta

--- services/meta-service.js
+++ services/meta-service.js
@@ -2,7 +2,7 @@ var config = require('../config/config');
 var Subject = require('../models/subject').Subject;
 
 exports.getSubjects = function() {
-    
+
     var query = Subject.find();
     query.sort({name: 1});
     query.select('_id name_et name_en');
@@ -41,63 +41,3 @@ exports.getDisplays = function() {
     { _id: 5, name: "other", icon: "other_display.png" }
   ];
 };
-
-
-/* OLD
-exports.getDisplays = function(next) {
-    // 0 > 2
-    // 1 > 4
-    // 2 > 3
-    // 3 > NO TRANSFER
-  return next(null, [
-    { _id: 0, name: "Arvuti (Computer)", icon: "pc.png" },
-    { _id: 1, name: "Nutitelefon (Smartphone)", icon: "smartphone.png" },
-    { _id: 2, name: "Tahvelarvuti (Tablet)", icon: "tablet.png" },
-    { _id: 3, name: "Muu (Other)", icon: "other_display.png" }
-  ]);
-};*/
-
-
-/* exports.insertSubjects = function(next) {
-
-  var subjects = [{ name: "Eesti keel " },
-  { name: "Vene keel emakeelena" },
-  { name: "Kirjandus" },
-  { name: "Eesti keel võõrkeelena" },
-  { name: "Inglise keel" },
-  { name: "Prantsuse keel" },
-  { name: "Saksa keel" },
-  { name: "Vene keel" },
-  { name: "Rootsi keel" },
-  { name: "Soome keel" },
-  { name: "Matemaatika" },
-  { name: "Loodusõpetus" },
-  { name: "Bioloogia" },
-  { name: "Geograafia" },
-  { name: "Füüsika" },
-  { name: "Keemia" },
-  { name: "Inimeseõpetus" },
-  { name: "Ajalugu" },
-  { name: "Ühiskonnaõpetus" },
-  { name: "Kunst" },
-  { name: "Muusika" },
-  { name: "Töö- ja tehnoloogiaõpetus" },
-  { name: "Käsitöö" },
-  { name: "Kodundus" },
-  { name: "Haridustehnoloogia" },
-  { name: "Kehaline kasvatus" },
-  { name: "Informaatika" },
-  { name: "Majandus ja ettevõtlus" },
-  { name: "Meediaõpetus" },
-  { name: "Rigiikaitse" },
-  { name: "Uurimistöö" },
-  { name: "Filosoofia" }];
-
-  Subject.create(subjects, function (err, subjectss) {
-    if(err){return err;}
-
-    return next(null, { subjects: subjectss });
-
-  });
-
-};*/
